560d8bbe78475796e8c1e8efbf2e2f07a7b5e7e4
kaimai33 <158132405+kaimai33@users.noreply.github.com>
2024-07-30 09:55:45 -0700

    Time: 446 ms (80.64%), Space: 30.5 MB (87.96%) - LeetHub

--- 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.py
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.py
@@ -7,7 +7,7 @@ class Solution:
         def binSearch(l, r):
             while l < r:
                 m = (l + r) // 2
-                if arr[m] < arr[m+1]:
+                if arr[m] < arr[m + 1]:
                     l = m + 1
                 else:
                     r = m
