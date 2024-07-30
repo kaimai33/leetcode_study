a722c4a11e6f59c677018aaa7e76e8980e59f0c5
kaimai33 <158132405+kaimai33@users.noreply.github.com>
2024-07-30 09:44:01 -0700

    Time: 450 ms (68.85%), Space: 30.6 MB (59.65%) - LeetHub

---
+++ 0882-peak-index-in-a-mountain-array/0882-peak-index-in-a-mountain-array.py
@@ -0,0 +1,15 @@
+class Solution:
+    def peakIndexInMountainArray(self, arr: List[int]) -> int:
+        """ 
+        TP:
+        bin search, why is this a medium problem lol??
+        """
+        def binSearch(l, r):
+            while l < r:
+                m = (l + r) // 2
+                if arr[m] < arr[m+1]:
+                    l = m + 1
+                else:
+                    r = m
+            return l
+        return binSearch(0, len(arr) - 1)
