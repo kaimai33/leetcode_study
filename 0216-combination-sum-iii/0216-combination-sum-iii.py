9bb2e2e02ec8a06719f1a8bf780f89af65344fe7
kaimai33 <158132405+kaimai33@users.noreply.github.com>
2024-07-31 08:00:19 -0700

    Time: 37 ms (50.43%), Space: 16.6 MB (21.56%) - LeetHub

---
+++ 0216-combination-sum-iii/0216-combination-sum-iii.py
@@ -0,0 +1,19 @@
+class Solution:
+    def combinationSum3(self, k: int, n: int) -> List[List[int]]:
+        ans = []
+
+        def dfs(k, n, curr_val, path):
+            # successful end case
+            if k == 0 and n == 0:
+                ans.append(path)
+                return
+            # unsuccessful end case
+            elif k == 0 or n < 0:
+                return
+
+            for i in range(curr_val, 10):
+                # we do i + 1 because we cannot repeat usage of digits
+                dfs(k - 1, n - i, i + 1, path + [i])
+        
+        dfs(k, n, 1, [])
+        return ans
