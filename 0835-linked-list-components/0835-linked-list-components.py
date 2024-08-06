5cd8b5fd7f9077931088d960cc26a4cca8f63fb1
kaimai33 <158132405+kaimai33@users.noreply.github.com>
2024-08-06 11:37:16 -0400

    Time: 67 ms (96.42%), Space: 20.9 MB (6.49%) - LeetHub

---
+++ 0835-linked-list-components/0835-linked-list-components.py
@@ -0,0 +1,22 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def numComponents(self, head: Optional[ListNode], nums: List[int]) -> int:
+        isConnected = False
+        # curr_subset = set()
+        nums_set = set(nums)
+        ans = 0
+        while head:
+            if head.val in nums_set:
+                # curr_subset.add()
+                isConnected = True
+            if head.val not in nums_set and isConnected:
+                isConnected = False
+                ans += 1
+            head = head.next
+        if isConnected:
+            ans += 1
+        return ans
