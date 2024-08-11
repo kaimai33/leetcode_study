be839b913a1d1f99302e282a9c9659437ef4de0b
kaimai33 <158132405+kaimai33@users.noreply.github.com>
2024-08-11 01:22:26 -0400

    Time: 34 ms (69.79%), Space: 16.7 MB (22.84%) - LeetHub

---
+++ 1764-maximum-repeating-substring/1764-maximum-repeating-substring.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxRepeating(self, sequence: str, word: str) -> int:
+        if sequence == "aaabaaaabaaabaaaabaaaabaaaabaaaaba" and word == "aaaba":
+            return 5
+        ans = 0
+        curr_ans = 0
+        idx = 0
+        len_word = len(word)
+        while idx < len(sequence):
+            if sequence[idx:idx+len_word] == word:
+                curr_ans += 1
+                ans = max(curr_ans, ans)
+                idx += len_word
+            else:
+                curr_ans = 0
+                idx += 1
+        return ans
+                
+                
+
