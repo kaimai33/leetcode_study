8ec775a3b57ba4ae875b1c3e6ca3ed1ba3e315af
kaimai33 <158132405+kaimai33@users.noreply.github.com>
2024-08-11 15:06:59 -0400

    Time: 48 ms (29.68%), Space: 16.6 MB (5.26%) - LeetHub

--- 2476-check-distances-between-same-letters/2476-check-distances-between-same-letters.py
+++ 2476-check-distances-between-same-letters/2476-check-distances-between-same-letters.py
@@ -1,13 +1,5 @@
 class Solution:
     def checkDistances(self, s: str, distance: List[int]) -> bool:
-        # ans = [0] * 26
-        # for i in range(len(s)):
-        #     if ans[ord(s[i]) - ord('a')] == 0:
-        #         ans[ord(s[i]) - ord('a')] -= i
-        #     else:
-        #         ans[ord(s[i]) - ord('a')] += i + 1
-        # return ans == distance
-
         hmap = {}
         for i in range(len(s)):
             if s[i] not in hmap:
