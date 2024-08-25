71dd42b83dfe954a07be363c510a0d91fde5c2e8
kaimai33 <158132405+kaimai33@users.noreply.github.com>
2024-08-25 07:48:22 -0400

    Time: 1172 ms (16.15%), Space: 16.6 MB (11.39%) - LeetHub

--- 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/1553-count-triplets-that-can-form-two-arrays-of-equal-xor.py
+++ 1553-count-triplets-that-can-form-two-arrays-of-equal-xor/1553-count-triplets-that-can-form-two-arrays-of-equal-xor.py
@@ -1,5 +1,13 @@
 class Solution:
     def countTriplets(self, arr: List[int]) -> int:
+        """
+        Steps:
+        1. create prefix xor
+        2. iterate through array
+        3. iterate on left side of the tracker
+        4. iterate on right side of the tracker
+        5. compare vals
+        """
         prefix_xor = [0]
         ans = 0
 
