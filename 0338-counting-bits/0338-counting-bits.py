1db2914e517e1c2c18b8e181923fb47325156ab7
kaimai33 <158132405+kaimai33@users.noreply.github.com>
2024-10-07 15:21:08 -0400

    Time: 59 ms (75.07%), Space: 23.2 MB (50.4%) - LeetHub

---
+++ 0338-counting-bits/0338-counting-bits.py
@@ -0,0 +1,9 @@
+class Solution:
+    def countBits(self, n: int) -> List[int]:
+        # saw answer online but I understand it
+        ans = [0] * (n + 1)
+        for i in range(1, n + 1):
+            # i & 2 finds the rightmost (least sig) digit and returns if it is 1 or 0
+            ans[i] = ans[i // 2] + (i & 1)
+        
+        return ans
